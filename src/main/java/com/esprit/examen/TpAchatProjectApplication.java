6e7862b687ef1efeab53f2227cc24473adb850af
ImenSaadFkihAhmed <101922854+ImenSaadFkihAhmed@users.noreply.github.com>
2023-12-04 18:06:49 +0100

    Update

--- src/main/java/com/esprit/examen/TpAchatProjectApplication.java
+++ src/main/java/com/esprit/examen/TpAchatProjectApplication.java
@@ -1,6 +1,6 @@
 package com.esprit.examen;
 
-
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
