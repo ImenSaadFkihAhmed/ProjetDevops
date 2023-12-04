c1539421541e57e848cac69098209dc1f246724d
ImenSaadFkihAhmed <101922854+ImenSaadFkihAhmed@users.noreply.github.com>
2023-12-04 17:51:11 +0100

    Update

--- src/main/java/com/esprit/examen/TpAchatProjectApplication.java
+++ src/main/java/com/esprit/examen/TpAchatProjectApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-
+@ComponentScan(basePackages = "com.example")
 @EnableScheduling
 @SpringBootApplication
 public class TpAchatProjectApplication {
